48c9bb366aa385b99e869946692bb8df49771f2f
Aierken Shalayiding <as4824@cs.rit.edu>
2022-05-04 00:07:44 -0400

    done

---
+++ Python_files/BLSTM_predic.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1' # Allow only 1 GPU visible to CUDA
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # Disable annoying logs
+
+
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models , losses
+# import simpleaudio as sa
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.models import load_model
+# from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+
+
+# In[2]:
+
+
+def _parseme(raw_audio_record):
+	feature_description = {
+	    'note': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+	    'note_str': tf.io.FixedLenFeature([], tf.string, default_value=''),
+	    'instrument': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+	    'instrument_str': tf.io.FixedLenFeature([], tf.string, default_value=''),
+	    'pitch': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+	    'velocity': tf.io.FixedLenFeature([], tf.int64,default_value=0),
+	    'sample_rate': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+	    'audio': tf.io.FixedLenSequenceFeature([], tf.float32,  allow_missing=True, default_value=0.0),
+	    'qualities': tf.io.FixedLenSequenceFeature([], tf.int64, allow_missing=True, default_value=0),
+	    'qualities_str': tf.io.FixedLenSequenceFeature([], tf.string, allow_missing=True, default_value=''),
+	    'instrument_family': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+	    'instrument_family_str': tf.io.FixedLenFeature([], tf.string, default_value=''),
+	    'instrument_source': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+	    'instrument_source_str': tf.io.FixedLenFeature([], tf.string, default_value='')     
+	}
+
+	return tf.io.parse_single_example(raw_audio_record, feature_description)
+
+
+
+def extract_data(ds_data,input_size,test_size):
+  x = []
+  y = []
+  count = 0
+  for element in ds_data:
+    count+=1
+    if count > test_size:
+        break;
+    if int(element['instrument_family'].numpy()) == 9:
+      continue
+    else:
+      temp = np.resize(element['audio'][:16000].numpy(),100*160)
+      x.append(temp)
+#       x.append(np.resize(element['audio'].numpy(),input_size*input_size))
+      if int(element['instrument_family'].numpy()) == 10:
+        y.append(element['instrument_family'].numpy()-1)
+      else :
+        y.append(element['instrument_family'].numpy())
+   
+  x = np.array(x)
+  y = np.array(y)
+  return x,y
+
+
+# In[3]:
+
+
+test_raw_data = tf.data.TFRecordDataset("/local/sandbox/nsynth-tf/nsynth-test.tfrecord")
+
+
+# In[4]:
+
+
+input_size = 16000
+test_size = 22679
+test_ar = test_raw_data.map(_parseme)
+x,y = extract_data(test_ar,input_size,test_size)
+x = np.expand_dims(x,axis=-1)
+
+
+# In[5]:
+
+
+complex_network_Model=load_model('project2_BLSTM_network.h5')
+complex_network_Model.summary()
+
+
+# In[6]:
+
+
+result =  complex_network_Model.evaluate(x, y)
+
+
+# In[7]:
+
+
+prediction = complex_network_Model.predict(x[:,:])
+
+
+# In[8]:
+
+
+# print(x.shape)
+# print(y.shape)
+# x_rechange = []
+# for i in x:    
+#     tmp = i[:,:,0]
+#     tmp = tmp.flatten()
+#     x_rechange.append(tmp)
+
+# x_rechange = np.array(x_rechange)
+# print(x_rechange.shape)
+# x = x_rechange
+
+
+# In[9]:
+
+
+print(prediction[0])
+print(y[0])
+print(len(y))
+print(len(prediction))
+
+
+# In[10]:
+
+
+true_table = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+miss_classified = []
+prediciton_by_class = []
+right_classified = []
+for i in range(len(prediction)):
+    prediciton_by_class.append(np.argmax(prediction[i]))
+    if y[i] == np.argmax(prediction[i]):
+        true_table[y[i]][0]+=1
+        right_classified.append([y[i],prediction[i],x[i],0])
+    else :
+        miss_classified.append([y[i],np.argmax(prediction[i])])
+        true_table[y[i]][1]+=1
+
+
+accuracy_set = []
+for i in range(len(true_table)):
+    right = true_table[i][0]
+    wrong = true_table[i][1]
+    accuracy = right/(right+wrong+1)
+    accuracy_set.append(accuracy)
+print(accuracy_set)
+data = [0,1,2,3,4,5,6,7,8,9]
+plt.hist(data,weights= accuracy_set,color = 'lightblue',ec="red")
+plt.savefig('project2_BLSTM_histogram.png')
+plt.figure().clear()
+print(accuracy_set)
+disp = ConfusionMatrixDisplay(confusion_matrix(y, prediciton_by_class, labels=[i for i in range(10)]))
+disp.plot()
+plt.savefig('project2_BLSTM_confusionmatrix.png')
+plt.figure().clear()
+
+
+# In[11]:
+
+
+fig, axes = plt.subplots(5, 2, figsize=(17, 19))
+visited = []
+for i in right_classified:
+    visited.append( [i[1].max(),i[0],i[2]] )
+visited = sorted(visited, key=lambda test: test[0],reverse=True)
+
+
+for clas in range(0,10):
+    r = clas // 2
+    c = clas % 2
+    ax = axes[r][c]
+    count = 0
+    for i in visited:
+        if i[1] == clas:
+          if count < 5:
+            count+=1
+            ax.plot(i[2])
+            ax.set_title('classes:'+str(clas))
+plt.show()
+plt.savefig('project2_BLSTM_high_prob_waveform.png')
+
+
+# In[12]:
+
+
+fig, axes = plt.subplots(5, 2, figsize=(17, 19))
+visited = []
+for i in right_classified:
+    visited.append( [i[1].max(),i[0],i[2]] )
+visited = sorted(visited, key=lambda test: test[0])
+
+for clas in range(0,10):
+    r = clas // 2
+    c = clas % 2
+    ax = axes[r][c]
+    count = 0
+    for i in visited:
+        if i[1] == clas:
+          if count < 5:
+            count+=1
+            ax.plot(i[2])
+            ax.set_title('classes:'+str(clas))
+plt.show()
+plt.savefig('project2_BLSTM_low_prob_waveform.png')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Python_files/train_BLSTM.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '6' # Allow only 1 GPU visible to CUDA
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # Disable annoying logs
+
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras import datasets, layers, models , losses
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# In[2]:
+
+
+DATA_PATH = '/local/sandbox/nsynth-tf'
+TEST_DATA = DATA_PATH + '/nsynth-test.tfrecord'
+TRAIN_DATA = DATA_PATH + '/nsynth-train.tfrecord'
+VALIDATION_DATA = DATA_PATH + '/nsynth-valid.tfrecord'
+
+BATCH_SIZE = 100
+EPOCHS = 5
+SHUFFLE_BUFFER = 150000
+#SHUFFLE_BUFFER = 289205
+
+
+# In[3]:
+
+
+def parse_data(raw_audio_record):
+	# Schema for encoded data
+	feature_description = {
+			# 'note': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+			# 'note_str': tf.io.FixedLenFeature([], tf.string, default_value=''),
+			# 'instrument': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+			# 'instrument_str': tf.io.FixedLenFeature([], tf.string, default_value=''),
+			# 'pitch': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+			# 'velocity': tf.io.FixedLenFeature([], tf.int64,default_value=0),
+			# 'sample_rate': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+			'audio': tf.io.FixedLenSequenceFeature([], tf.float32,  allow_missing=True, default_value=0.0),
+			# 'qualities': tf.io.FixedLenSequenceFeature([], tf.int64, allow_missing=True, default_value=0),
+			# 'qualities_str': tf.io.FixedLenSequenceFeature([], tf.string, allow_missing=True, default_value=''),
+			'instrument_family': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+			# 'instrument_family_str': tf.ioqui.FixedLenFeature([], tf.string, default_value=''),
+			# 'instrument_source': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+			# 'instrument_source_str': tf.io.FixedLenFeature([], tf.string, default_value='')     
+	}
+	# record = tf.io.parse_single_example(raw_audio_record, feature_description)
+	# return record['audio'], record['instrument_family']
+	return tf.io.parse_single_example(raw_audio_record, feature_description)
+
+
+# In[4]:
+
+
+raw_train_data = tf.data.TFRecordDataset(TRAIN_DATA)
+raw_test_data = tf.data.TFRecordDataset(TEST_DATA)
+raw_val_data = tf.data.TFRecordDataset(VALIDATION_DATA)
+
+# Decode and trim each record to only contain audio and instrument_family attributes
+train_data = raw_train_data.map(parse_data)
+test_data = raw_test_data.map(parse_data)
+val_data = raw_val_data.map(parse_data)
+
+# Filter out family #9 (synth lead)
+train_data = train_data.filter(lambda x: x['instrument_family'] != 9)
+test_data = test_data.filter(lambda x: x['instrument_family'] != 9)
+val_data = val_data.filter(lambda x: x['instrument_family'] != 9)
+
+
+
+# In[5]:
+
+
+# Shuffle
+train_data = train_data.shuffle(buffer_size=SHUFFLE_BUFFER, reshuffle_each_iteration=True)
+val_data = val_data.shuffle(buffer_size=SHUFFLE_BUFFER, reshuffle_each_iteration=True)
+
+
+# In[6]:
+
+
+AUDIO_END = 16000
+def ten_to_nine(t):
+    if t == 10:
+        return tf.constant(9, dtype=tf.int64)
+    else:
+        return t
+
+train_data = train_data.map(
+    lambda x: (tf.reshape(x['audio'][:AUDIO_END], (16000, 1)),
+               tf.reshape(tf.py_function(func=ten_to_nine,inp=[x['instrument_family']], Tout=tf.int64), (1,))
+              ))
+    
+test_data = test_data.map(
+    lambda x: (tf.reshape(x['audio'][:AUDIO_END], (16000, 1)),
+               tf.reshape(tf.py_function(func=ten_to_nine,inp=[x['instrument_family']], Tout=tf.int64), (1,))
+              ))
+    
+val_data = val_data.map(
+    lambda x: (tf.reshape(x['audio'][:AUDIO_END], (16000, 1)),
+               tf.reshape(tf.py_function(func=ten_to_nine,inp=[x['instrument_family']], Tout=tf.int64), (1,))
+              ))
+
+
+# In[7]:
+
+
+# Shuffle training data and prepare it for input
+# train_data = train_data.shuffle(buffer_size=SHUFFLE_BUFFER, reshuffle_each_iteration=True)
+# val_data = val_data.shuffle(buffer_size=SHUFFLE_BUFFER, reshuffle_each_iteration=True)
+
+# Batch
+train_data = train_data.batch(BATCH_SIZE).cache().prefetch(BATCH_SIZE)
+val_data = val_data.batch(BATCH_SIZE).cache().prefetch(BATCH_SIZE)
+test_data = test_data.batch(BATCH_SIZE).cache().prefetch(BATCH_SIZE)
+
+
+# In[8]:
+
+
+# Build the model
+model = Sequential()
+
+# model.add(layers.Conv2D(32, (2, 2), padding='same', activation='relu', input_shape=(100, 160, 1)))
+# model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+model.add(layers.Conv1D(32, kernel_size=160, activation='relu', input_shape=(16000, 1)))
+model.add(layers.MaxPool1D(pool_size=4, strides=None, padding='valid'))
+
+model.add(layers.Bidirectional(layers.LSTM(50)))
+model.add(layers.Dropout(0.5))
+
+
+# model.add(layers.Dropout(0.25))
+# model.add(layers.Conv2D(32, (3, 3),activation='relu'))
+# model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+# model.add(layers.Dropout(0.5))
+# model.add(layers.Conv2D(128, (3, 3), padding='same',activation ='relu'))
+# model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+# model.add(layers.Dropout(0.5))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(256,activation='relu'))
+# model.add(layers.Dropout(0.5))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+print(model.summary())
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=['accuracy'])
+
+
+# In[9]:
+
+
+# Training
+history = model.fit(train_data, validation_data=val_data, epochs=5)
+#history = model.fit(val_data, epochs=1)
+
+
+# In[12]:
+
+
+# Testing
+test_loss, test_acc = model.evaluate(test_data, verbose=2)
+print(test_acc)
+
+
+# In[13]:
+
+
+model.save('project2_BLSTM_network.h5')
+print('project2_BLSTM_network.h5 Model Saved!')
+
+
+# In[14]:
+
+
+print(history.history['accuracy'])
+
+fig, axs = plt.subplots(2, 1, figsize=(15,15))  
+axs[0].plot(history.history['loss']) 
+axs[0].plot(history.history['val_loss']) 
+axs[0].title.set_text('Training Loss vs Validation Loss') 
+axs[0].legend(['Train', 'Val'])  
+
+axs[1].plot(history.history['accuracy']) 
+axs[1].plot(history.history['val_accuracy']) 
+axs[1].title.set_text('Training Accuracy vs Validation Accuracy') 
+axs[1].legend(['Train', 'Val'])
+plt.savefig('project2_simple_network.png')
+
+
+# In[ ]:
+
+
+
+
